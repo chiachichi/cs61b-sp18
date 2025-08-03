820b6e42611c60028901080007b9cd2a1b38e4c6
chiachichi <dreamin10118@gmail.com>
2025-08-04 00:00:13 +0800

    feat: use for loop to rewrite Maximum program

--- ecs/Maximum.java
+++ ecs/Maximum.java
@@ -1,16 +1,28 @@
 public class Maximum {
     /** 取得並印出非負整數陣列中的最大值 */
+    // public static int getMax(int[] array) {
+    //     int max = -1; // 表示目前的最大值，必須小於陣列第一個元素
+    //     int index = 0; // 表示陣列的索引，從0開始
+    //     int ele; // 表示陣列元素
+    //     // 使用 while 在陣列上迭代
+    //     while (index <= array.length - 1) { // 控制索引不要超過範圍
+    //         ele = array[index]; // 取出元素
+    //         if (ele > max) { // 比較大小
+    //             max = ele;
+    //         }
+    //         index = index + 1; // 更新索引
+    //     }
+    //     return max; // 回傳
+    // }
     public static int getMax(int[] array) {
         int max = -1; // 表示目前的最大值，必須小於陣列第一個元素
-        int index = 0; // 表示陣列的索引，從0開始
         int ele; // 表示陣列元素
-        // 使用 while 在陣列上迭代
-        while (index <= array.length - 1) { // 控制索引不要超過範圍
+        // 使用 for 在陣列上迭代
+        for (int index = 0; index < array.length; index = index + 1) {
             ele = array[index]; // 取出元素
             if (ele > max) { // 比較大小
                 max = ele;
             }
-            index = index + 1; // 更新索引
         }
         return max; // 回傳
     }
