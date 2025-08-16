0d77d0985247cd4afcf2beab5a9f8db7d433841a
chiachichi <dreamin10118@gmail.com>
2025-08-16 23:19:40 +0800

    completed leap year of lab1

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -1,7 +1,13 @@
 /** Class that determines whether or not a year is a leap year.
- *  @author YOUR NAME HERE
+ *  @author chiachichi
  */
 public class LeapYear {
+    /** Determines whether or not a year is a leap year.
+     *  Alg: A leap year is either divisible by 400 or (divisible by 4 and not by 100.)
+     */
+    public static boolean isLeapYear(int year) {
+        return (year % 400 == 0) || (year % 4 == 0 && year % 100 != 0);
+    }
 
     /** Calls isLeapYear to print correct statement.
      *  @param  year to be analyzed
@@ -18,7 +24,7 @@ public class LeapYear {
     public static void main(String[] args) {
         if (args.length < 1) {
             System.out.println("Please enter command line arguments.");
-            System.out.println("e.g. java Year 2000");
+            System.out.println("e.g. java LeapYear 2000");
         }
         for (int i = 0; i < args.length; i++) {
             try {
